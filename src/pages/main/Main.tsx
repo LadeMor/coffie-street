5e6cb5330c7f884162e4135b22a6f8b0359c725e
Sergiy <sergpyatko@gmail.com>
2024-09-03 21:36:12 +0300

    Added main coffie cup element on the preview

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,47 @@
+import Container from "../../components/container/Container";
 import Header from "../../components/header/Header";
+
+import "./Main.scss";
+
+import exclude from "../../assets/icons/Exclude.svg";
+import main_coffie from "../../assets/images/main-coffie.png";
+import rate_star from "../../assets/icons/akar-icons_star.svg";
 
 const Main = () => {
     return(
         <div>
             <Header/>
+            <section id="main-preview">
+                <Container>
+                    <div className="main-preview-block">
+                        <div className="main-preview-block-text">
+                            <h1>Enjoy your coffee before your activity</h1>
+                            <p>Boost your productivity and build your mood with a glass of coffee in the morning</p>
+                            <div className="main-preview-block-text-buttons">
+                                <button>Order now</button>
+                                <button>More menu</button>
+                            </div>
+                        </div>
+                        <div className="main-preview-block-image">
+                            <img src={exclude} alt="Exclude background" className="main-preview-block-image-exclude"/>
+                            <img src={main_coffie} alt="Coffie cup" className="main-preview-block-image-coffie-cup"/>
+                            <div className="coffie-label-border coffie-label-title">
+                                <h4>Cappuccino</h4>
+                                <div className="border"></div>
+                            </div>
+                            <div className="coffie-label-border coffie-label-rate">
+                                <h4>4.8</h4>
+                                <img src={rate_star}/>
+                                <div className="border"></div>
+                            </div>
+                            <div className="coffie-label-border coffie-label-count">
+                                <h4>18K</h4>
+                                <div className="border"></div>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </section>
         </div>
     );
 }
