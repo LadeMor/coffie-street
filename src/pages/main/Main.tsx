ae8a749c18de5c01f4f2077012598dba3c8b5f9a
Sergiy <sergpyatko@gmail.com>
2024-09-04 13:57:04 +0300

    Started coffie list scetion

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Container from "../../components/container/Container";
 import Header from "../../components/header/Header";
+import TransparentBorderBox from "../../components/transparent-border-box/TransparentBorderBox";
 
 import "./Main.scss";
 
@@ -13,41 +14,68 @@ import white_card_icon from "../../assets/icons/card-icon-white.svg";
 import bg_coffie_beans from "../../assets/images/bg-coffie-beans.png";
 
 
+import coffie_item1 from "../../assets/images/coffie-list/coffie1.png";
+import coffie_item2 from "../../assets/images/coffie-list/coffie2.png";
+import coffie_item3 from "../../assets/images/coffie-list/coffie3.png";
+
+
 const Main = () => {
-    return(
+    return (
         <React.Fragment>
-            <Header/>
+            <Header />
             <section id="main-preview">
                 <Container>
                     <div className="main-preview-block">
                         <div className="main-preview-block-text">
                             {/* Fix z-index of this block !!! */}
-                            <h1>Enjoy your <span className="coffie-focus">coffee</span> <br/> before your activity</h1>
-                            <p>Boost your productivity and build your<br/> mood with a glass of coffee in the morning</p>
-                            <div className="main-preview-block-text-buttons">
-                                <button className="order-btn">Order now<img src={white_card_icon} className="btn-white-card-icon"/></button>
-                                <button className="menu-btn">More menu</button>
+                            <div>
+                                <h1>Enjoy your <span className="coffie-focus">coffee</span> <br /> before your activity</h1>
+                                <p>Boost your productivity and build your<br /> mood with a glass of coffee in the morning</p>
+                                <div className="main-preview-block-text-buttons">
+                                    <button className="order-btn">Order now<img src={white_card_icon} className="btn-white-card-icon" /></button>
+                                    <button className="menu-btn">More menu</button>
+                                </div>
                             </div>
+                            <h1 className="section-title">
+                                Popular <span className="underline">now</span>
+                                <img src={bg_coffie_beans} className="bg-bottom-coffie-beans" />
+                            </h1>
                         </div>
                         <div className="main-preview-block-image">
-                            <img src={exclude} alt="Exclude background" className="main-preview-block-image-exclude"/>
-                            <img src={main_coffie} alt="Coffie cup" className="main-preview-block-image-coffie-cup"/>
-                            <div className="coffie-label-border coffie-label-title">
+                            <img src={exclude} alt="Exclude background" className="main-preview-block-image-exclude" />
+                            <img src={main_coffie} alt="Coffie cup" className="main-preview-block-image-coffie-cup" />
+                            <TransparentBorderBox className="coffie-label-title" borderRadius="42px">
                                 <h4>Cappuccino</h4>
-                                <div className="border"></div>
-                            </div>
-                            <div className="coffie-label-border coffie-label-rate">
+                            </TransparentBorderBox>
+                            <TransparentBorderBox className="coffie-label-rate" borderRadius="42px">
                                 <h4>4.8</h4>
-                                <img src={rate_star}/>
-                                <div className="border"></div>
-                            </div>
-                            <div className="coffie-label-border coffie-label-count">
+                                <img src={rate_star} />
+                            </TransparentBorderBox>
+                            <TransparentBorderBox className="coffie-label-count" borderRadius="42px">
                                 <h4>18K</h4>
-                                <div className="border"></div>
-                            </div>
-                            <img src={bg_coffie_beans} className="bg-top-coffie-beans"/>
+                            </TransparentBorderBox>
+                            <img src={bg_coffie_beans} className="bg-top-coffie-beans" />
                         </div>
-                        
+
+                    </div>
+                </Container>
+            </section>
+            <section id="coffie-list">
+                <Container>
+                    <div className="coffie-list-container">
+                        <div className="coffie-list-box">
+                            <div className="coffie-item">
+                                <TransparentBorderBox className="coffie-item-border-box" borderRadius="12px">
+                                    <div className="coffie-item-preview">
+                                        <img src={coffie_item1}/>
+                                        <TransparentBorderBox className="coffie-item-preview-rate" borderRadius="42px">
+                                            <h4>4.8</h4>
+                                            <img src={rate_star} />
+                                        </TransparentBorderBox> 
+                                    </div>
+                                </TransparentBorderBox>
+                            </div>
+                        </div>
                     </div>
                 </Container>
             </section>
