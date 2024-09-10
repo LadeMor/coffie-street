1b1ff542cedc783e96f85b2cbaa1465945af685b
Sergiy <sergpyatko@gmail.com>
2024-09-10 15:56:22 +0300

    Added animations to about-us

--- src/sections/about-us/AboutUs.tsx
+++ src/sections/about-us/AboutUs.tsx
@@ -1,3 +1,6 @@
+import { useRef } from "react";
+import { useInView, motion } from "framer-motion";
+
 import Container from "../../components/container/Container";
 import TransparentBorderBox from "../../components/transparent-border-box/TransparentBorderBox";
 
@@ -6,16 +9,32 @@ import "./AboutUs.scss";
 import about_up_cup from "../../assets/images/about-us/About us img.png";
 import about_us_exclude from "../../assets/images/about-us/Exclude multiple.svg";
 
+
+
 const AboutUs = () => {
+
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+
     return(
         <section id="about-us">
                 <Container>
-                    <div className="about-us-main-wrapper">
+                    <div className="about-us-main-wrapper" ref={ref}>
                         <div className="about-us-wrapper">
-                            <TransparentBorderBox className="about-us-image-wrapper" borderRadius="10px">
+                            <TransparentBorderBox className="about-us-image-wrapper" borderRadius="10px" style={{
+                                transform: isInView ? "none" : "translateX(-200px)",
+                                opacity: isInView ? 1 : 0,
+
+                                transition: `all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) ${0.5}s`
+                            }}>
                                     <img src={about_up_cup} alt="Coffie cup"/>
                             </TransparentBorderBox>
-                            <div className="about-us-info">
+                            <div className="about-us-info" style={{
+                                transform: isInView ? "none" : "translateY(20px)",
+                                opacity: isInView ? 1 : 0,
+
+                                transition: `all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) ${0.5}s`
+                            }}>
                                     <h1>About <span className="underline">us</span></h1>
                                     <h2>We provide quality coffee, and ready to deliver.</h2>
                                     <p>We are a company that makes and distributes delicious drinks.
@@ -23,11 +42,6 @@ const AboutUs = () => {
                                     <button>Get your coffie</button>
                             </div>
                         </div>
-                        
-                        {/* <div className="about-us-exclude-wrapper">
-                            <img src={about_us_exclude} alt="Coffie cups" className="about-us-exclude"/>
-                        </div> */}
-                        {/* <img src={about_us_exclude} alt="Coffie cups" className="about-us-exclude"/> */}
                     </div>
                     <div className="about-us-exclude-wrapper">
                             <img src={about_us_exclude} alt="Coffie cups" className="about-us-exclude"/>
